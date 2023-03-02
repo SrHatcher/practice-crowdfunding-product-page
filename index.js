a06eaa52a013e3ee912709a965f6acff2de19aa8
SrHatcher <erickomaldonado11@gmail.com>
2023-03-01 20:55:34 -0600

    still working on mobile version

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const menuButton = document.querySelector('.header__button')
 const headerNavbar = document.querySelector('.header__navbar')
 const header = document.getElementById('header')
+const mainHeading = document.querySelector('.main__heading')
 
 function openMenu(){
     headerNavbar.classList.toggle('header__navbar--inactive')
     menuButton.classList.toggle('header__button--active')
     header.classList.toggle('header--active')
+    mainHeading.classList.toggle('main__heading--corrector')
 }
 
 menuButton.addEventListener('click', openMenu)
