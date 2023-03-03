bfc7a674f3643efdfff7fe19c0162fc87f702618
SrHatcher <erickomaldonado11@gmail.com>
2023-03-03 14:23:45 -0600

    first modal: select pledge completed, now working on second modal: thank you view

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const menuButton = document.querySelector('.header__button')
 const headerNavbar = document.querySelector('.header__navbar')
 const header = document.getElementById('header')
 const mainHeading = document.querySelector('.main__heading')
+const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.modal__close')
+const backButton = document.querySelector('.heading__back_button')
+
+const totalHeight = document.body.scrollHeight
+modal.style.height = `${totalHeight}px`
+
+function openModal(){
+    modal.classList.toggle('modal--inactive')
+}
+
+function closeModal(){
+    modal.classList.toggle('modal--inactive')
+}
 
 function openMenu(){
     headerNavbar.classList.toggle('header__navbar--inactive')
@@ -10,4 +24,6 @@ function openMenu(){
     mainHeading.classList.toggle('main__heading--corrector')
 }
 
+backButton.addEventListener('click', openModal)
 menuButton.addEventListener('click', openMenu)
+modalClose.addEventListener('click', closeModal)
